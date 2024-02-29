93f5e1ee83b6d985fe3e041cdc9cba8b5d823e87
Jon0710 <jtmendoza2@student.apc.edu.ph>
2024-02-29 22:10:07 +0800

    php part1 variable

--- week8/index.php
+++ week8/index.php
@@ -1,10 +1,25 @@
 <!DOCTYPE html>
 <html>
 <body>
+<h1>PHP Lessons</h1>
+    <div>
+    <h2>PHP Intro</h2>
+    <?php
+    echo "My first PHP script!";
+    ?>
+    </div>
 
+
+<div>
+<h2>PHP Variable</h2>
 <?php
-echo "My first PHP script!";
-?>
+$x = 5;
+$y = "John";
 
+echo $x;
+echo "<br>";
+echo $y;
+?>
+</div>
 </body>
 </html>
