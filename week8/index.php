c10c366233343b667e43ae768cd7a8e7b05c3d46
Jon0710 <jtmendoza2@student.apc.edu.ph>
2024-02-29 22:17:39 +0800

    php part1 echo/print

--- week8/index.php
+++ week8/index.php
@@ -21,5 +21,15 @@ echo "<br>";
 echo $y;
 ?>
 </div>
+
+<h2>Echo/Print</h2>
+<?php
+echo "PHP is Enjoyable!<br>";
+echo "Hello world!<br>";
+echo "I'm going to learn PHP!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.<br>";
+print "Hi, I'm using PHP scripting language<br>";
+print "Print was used to execute this statement.";
+?> 
 </body>
 </html>
